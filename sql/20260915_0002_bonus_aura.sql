b092cdffc5424ecc14863d5bf8f757dbbdff37ad
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2026-09-15 14:39:41 +0200

    ajout de nouvelles auras

---
+++ sql/20260915_0002_bonus_aura.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.bonus_type(tbonus_libc, tonbus_libelle, tbonus_gentil_positif, tbonus_nettoyable, tbonus_cumulable, tbonus_degressivite, tbonus_description, tbonus_compteur, tbonus_aura, tbonus_aura_libc, tbonus_aura_vmax)
+VALUES ('@TO', 'Égide de martialité', true, 'N', 'N', null, 'Aura de protection contre le malus de martialité', 'N', true, 'TOU', 0);
