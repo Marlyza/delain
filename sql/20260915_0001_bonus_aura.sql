a1accb5c19bbe03aa9a615a3720800752c1efb05
AUBOURG Ludovic <LAUBOURG@fr.scc.com>
2026-09-15 10:22:35 +0200

    ajout de nouvelles auras

---
+++ sql/20260915_0001_bonus_aura.sql
@@ -0,0 +1,22 @@
+
+
+INSERT INTO public.bonus_type(tbonus_libc, tonbus_libelle, tbonus_gentil_positif, tbonus_nettoyable, tbonus_cumulable, tbonus_degressivite, tbonus_description, tbonus_compteur, tbonus_aura, tbonus_aura_libc, tbonus_aura_vmax)
+VALUES ('@AT', 'Égide d''Attaque', true, 'N', 'N', null, 'Aura de protection contre le malus d''Attaque', 'N', true, 'ATT', 0);
+
+INSERT INTO public.bonus_type(tbonus_libc, tonbus_libelle, tbonus_gentil_positif, tbonus_nettoyable, tbonus_cumulable, tbonus_degressivite, tbonus_description, tbonus_compteur, tbonus_aura, tbonus_aura_libc, tbonus_aura_vmax)
+VALUES ('@DG', 'Bouclier de Ravage', true, 'N', 'N', null, 'Aura de protection contre le malus de Ravage', 'N', true, 'DEG', 0);
+
+INSERT INTO public.bonus_type(tbonus_libc, tonbus_libelle, tbonus_gentil_positif, tbonus_nettoyable, tbonus_cumulable, tbonus_degressivite, tbonus_description, tbonus_compteur, tbonus_aura, tbonus_aura_libc, tbonus_aura_vmax)
+VALUES ('@MV', 'Sceau de Vélocité', true, 'N', 'N', null, 'Aura de protection contre le malus de Mobilité', 'N', true, 'DEP', 0);
+
+INSERT INTO public.bonus_type(tbonus_libc, tonbus_libelle, tbonus_gentil_positif, tbonus_nettoyable, tbonus_cumulable, tbonus_degressivite, tbonus_description, tbonus_compteur, tbonus_aura, tbonus_aura_libc, tbonus_aura_vmax)
+VALUES ('@PA', 'Bouclier d''Ardeur', true, 'N', 'N', null, 'Aura de protection contre le malus d''Ardeur', 'N', true, 'PAA', 0);
+
+INSERT INTO public.bonus_type(tbonus_libc, tonbus_libelle, tbonus_gentil_positif, tbonus_nettoyable, tbonus_cumulable, tbonus_degressivite, tbonus_description, tbonus_compteur, tbonus_aura, tbonus_aura_libc, tbonus_aura_vmax)
+VALUES ('@EQ', 'Aura de reflexes', true, 'N', 'N', null, 'Aura de protection contre le malus d''Esquive', 'N', true, 'ESQ', 0);
+
+INSERT INTO public.bonus_type(tbonus_libc, tonbus_libelle, tbonus_gentil_positif, tbonus_nettoyable, tbonus_cumulable, tbonus_degressivite, tbonus_description, tbonus_compteur, tbonus_aura, tbonus_aura_libc, tbonus_aura_vmax)
+VALUES ('@AR', 'Sceau de Cuirasse', true, 'N', 'N', null, 'Aura de protection contre le malus d''armure', 'N', true, 'ARM', 0);
+
+INSERT INTO public.bonus_type(tbonus_libc, tonbus_libelle, tbonus_gentil_positif, tbonus_nettoyable, tbonus_cumulable, tbonus_degressivite, tbonus_description, tbonus_compteur, tbonus_aura, tbonus_aura_libc, tbonus_aura_vmax)
+VALUES ('@RG', 'Aura de Vigueur', true, 'N', 'N', null, 'Aura de protection contre le malus de Régénération', 'N', true, 'REG', 0);
